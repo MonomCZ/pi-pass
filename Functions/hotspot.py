aa0387c2b0a3cf3ddaf8ba72acdaea867edb2f90
MonomCZ <vitek.monom@gmail.com>
2026-07-26 19:17:51 +0200

    Add deletion of existing hotspot connection before creating a new one

--- Functions/hotspot.py
+++ Functions/hotspot.py
@@ -4,6 +4,8 @@ def start_hotspot():
     interface = 'wlan0'
     hotspot_name = 'MyHotspot'
 
+    subprocess.run(['sudo','nmcli', 'connection', 'delete', hotspot_name], stderr=subprocess.DEVNULL)
+
     subprocess.run(['sudo','nmcli', 'connection', 'add', 'type', 'wifi', 'ifname', interface, 'con-name', hotspot_name,'ssid', hotspot_name])
 
     subprocess.run(['sudo','nmcli', 'connection', 'modify', hotspot_name,'802-11-wireless.mode', 'ap','ip4.method', 'shared', 'ipv6.method', 'ignore'])
