cf2bf6db16cad027c2b055e4efd2044d44479eec
MonomCZ <vitek.monom@gmail.com>
2026-07-26 19:15:44 +0200

    Fix hotspot configuration to remove security settings instead of mode

--- Functions/hotspot.py
+++ Functions/hotspot.py
@@ -8,6 +8,6 @@ def start_hotspot():
 
     subprocess.run(['sudo','nmcli', 'connection', 'modify', hotspot_name,'802-11-wireless.mode', 'ap','ip4.method', 'shared', 'ipv6.method', 'ignore'])
 
-    subprocess.run(['sudo','nmcli', 'connection', 'modify', hotspot_name,'remove', '802-11-wireless.mode'])
+    subprocess.run(['sudo','nmcli', 'connection', 'modify', hotspot_name,'remove', '802-11-wireless-security'])
 
     subprocess.run(['sudo','nmcli', 'connection', 'up', hotspot_name])
