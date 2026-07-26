47bf67b48abf2951cfbed53e7cd5c318ca1e8dea
MonomCZ <vitek.monom@gmail.com>
2026-07-26 19:14:28 +0200

    Refactor start_hotspot function to remove hotspot before adding a new one

--- Functions/hotspot.py
+++ Functions/hotspot.py
@@ -4,20 +4,10 @@ def start_hotspot():
     interface = 'wlan0'
     hotspot_name = 'MyHotspot'
 
-    subprocess.run(['sudo', 'nmcli', 'connection', 'delete', hotspot_name], stderr=subprocess.DEVNULL)
+    subprocess.run(['sudo','nmcli', 'connection', 'add', 'type', 'wifi', 'ifname', interface, 'con-name', hotspot_name,'ssid', hotspot_name])
 
-    subprocess.run([
-        'sudo', 'nmcli', 'connection', 'add',
-        'type', 'wifi',
-        'ifname', interface, 'con-name', hotspot_name,
-        'ssid', hotspot_name
-    ])
+    subprocess.run(['sudo','nmcli', 'connection', 'modify', hotspot_name,'802-11-wireless.mode', 'ap','ip4.method', 'shared', 'ipv6.method', 'ignore'])
 
-    subprocess.run([
-        'sudo', 'nmcli', 'connection', 'modify', hotspot_name,
-        '802-11-wireless.mode', 'ap',
-        '802-11-wireless.band', 'bg',
-        '802-11-wireless-security.key-mgmt', ''
-    ])
+    subprocess.run(['sudo','nmcli', 'connection', 'modify', hotspot_name,'remove', '802-11-wireless.mode'])
 
-    subprocess.run(['sudo', 'nmcli', 'connection', 'up', hotspot_name])
+    subprocess.run(['sudo','nmcli', 'connection', 'up', hotspot_name])
