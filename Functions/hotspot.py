ca9a11f3cdab88281f2d519e016390d496370883
MonomCZ <vitek.monom@gmail.com>
2026-07-26 22:14:58 +0200

    Refactor hotspot setup to streamline IP configuration and remove redundant commands

--- Functions/hotspot.py
+++ Functions/hotspot.py
@@ -7,15 +7,16 @@ def start_hotspot():
     subprocess.run(['sudo','nmcli', 'connection', 'delete', hotspot_name], stderr=subprocess.DEVNULL)
     #creates a new one
     subprocess.run(['sudo','nmcli', 'connection', 'add', 'type', 'wifi', 'ifname', interface, 'con-name', hotspot_name,'ssid', hotspot_name], check=True)
+    #sets the ip (so it doesnt change so the captive portal thing doesnt break)
+    subprocess.run(['sudo','nmcli', 'connection', 'modify', hotspot_name,'ipv4.addresses', '10.42.0.1/24'], check=True)
+    subprocess.run(['sudo','nmcli', 'connection', 'modify', hotspot_name,'ipv4.gateway', '10.42.0.1','ipv4.dns', '10.42.0.1','ipv4.method', 'manual', ], check=True)
     #does *something* 
-    subprocess.run(['sudo','nmcli', 'connection', 'modify', hotspot_name,'802-11-wireless.mode', 'ap','ipv4.method', 'manual', 'ipv6.method', 'ignore'], check=True)
+    subprocess.run(['sudo','nmcli', 'connection', 'modify', hotspot_name,'802-11-wireless.mode', 'ap','ipv6.method', 'ignore'], check=True)
     #makes it so the hotspot doesn't automatically start on boot
     subprocess.run(['sudo', 'nmcli', 'connection', 'modify', hotspot_name,'connection.autoconnect', 'no'], check=True)
     #removes the password :3
     subprocess.run(['sudo','nmcli', 'connection', 'modify', hotspot_name,'remove', '802-11-wireless-security'], check=True)
-    #sets the ip (so it doesnt change so the captive portal thing doesnt break)
-    subprocess.run(['sudo','nmcli', 'connection', 'modify', hotspot_name,'ipv4.addresses', '10.42.0.1/24'], check=True)
-    subprocess.run(['sudo','nmcli', 'connection', 'modify', hotspot_name,'ipv4.gateway', '10.42.0.1','ipv4.dns', '10.42.0.1'], check=True)
+
     #just starts it up
     subprocess.run(['sudo','nmcli', 'connection', 'up', hotspot_name], check=True)
 
