138a61af2586b0b361d3c475dcee407b5f1b0724
MonomCZ <vitek.monom@gmail.com>
2026-07-28 21:05:29 +0200

    Refactor GPIO button handling: move button1 function to gpio_input.py and update testing script to use new import

--- _TESTING.py
+++ _TESTING.py
@@ -1,6 +1,9 @@
 import Functions.oled_display as oled_display
+import Functions.gpio_input as gpio_input
 import time
 while True:
+    if gpio_input.button1():
+        oled_display.display_text("Button 1 pressed", 0)
     oled_display.clear()
     oled_display.display_temperature()
     oled_display.show()
